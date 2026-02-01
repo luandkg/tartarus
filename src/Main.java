d66263fc1dbbd7c666da8a198959121a70294d58
Luan Freitas <luandkg@gmail.com>
2026-02-01 09:38:47 -0300

    🎉 init(tartarus): Iniciando projeto com meu irmão @CalebeAF02

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Calebe - vc deve implementar uma estrutura de dados lista
+        // O nome da classe deve ser Lista
+        // deve ter as funções getQuantidade() e adicionar()
+
+    }
+}
