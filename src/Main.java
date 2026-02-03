7459f694f6683beb69476363a1a5a32146282b9f
CalebeAF02 <calebe.2324@gmail.com>
2026-02-03 15:36:27 -0300

    feat(ListaSimples): add e dell do inicio .

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.xml.transform.stream.StreamSource;
 
 public class Main {
     public static void main(String[] args) {
@@ -5,6 +6,23 @@ public class Main {
         // Calebe - vc deve implementar uma estrutura de dados lista
         // O nome da classe deve ser Lista
         // deve ter as funções getQuantidade() e adicionar()
+
+        ListaSimples l =  new ListaSimples();
+        System.out.println(l);
+        l.inserirInicio("primeira");
+        System.out.println(l);
+        l.inserirInicio("segunda");
+        System.out.println(l);
+        l.inserirInicio("terceira");
+        System.out.println(l);
+
+        System.out.println("Tamanho : " + l.getQuantidade());
+
+        while (l.retirarInicio() != null) {
+            System.out.println(l);
+        }
+
+        System.out.println("Tamanho : " + l.getQuantidade());
 
     }
 }
---
+++ src/No.java
@@ -0,0 +1,4 @@
+public class No {
+    String info;
+    No proximo;
+}
