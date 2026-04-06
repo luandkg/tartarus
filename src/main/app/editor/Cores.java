7bed86ed8b0ea7b44b3b9d524936fb931c04360f
CalebeAF02 <calebe.2324@gmail.com>
2026-04-06 15:17:21 -0300

    feat(Cores): Implementa uma classe chamada Cores que tem os atributos estáticos constantes do tipo Cor :
    -Amarelo
    -Vermelho
    -Verde
    -Azul
    -Laranja
    -Preto
    -Branco
    -Cinza
    -Marrom
    -Magenta
    -Rosa
    -Ciano
    
    closes #47

---
+++ src/main/app/editor/Cores.java
@@ -0,0 +1,16 @@
+package main.app.editor;
+
+public class Cores {
+    public static Cor Amarelo = new Cor(0,255, 235, 59);
+    public static Cor Vermelho = new Cor(0,244, 67, 54);
+    public static Cor Verde = new Cor(0,76, 175, 80);
+    public static Cor Azul = new Cor(0,33, 150, 243);
+    public static Cor Laranja = new Cor(0,255, 152, 0);
+    public static Cor Preto = new Cor(0,33, 33, 33);
+    public static Cor Branco = new Cor(0,250, 250, 250);
+    public static Cor Cinza = new Cor(0,158, 158, 158);
+    public static Cor Marrom = new Cor(0,93, 64, 55);
+    public static Cor Magenta = new Cor(0,156, 39, 176);
+    public static Cor Rosa = new Cor(0,240, 98, 146);
+    public static final Cor CIANO = new Cor(0,38, 198, 218);
+}
