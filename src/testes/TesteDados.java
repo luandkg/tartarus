df8bc2a75c31df3cbe7571121b13c4b21493f17e
CalebeAF02 <calebe.2324@gmail.com>
2026-04-07 11:25:43 -0300

    refactor(Testes): Comentando ocasiões onde se abri e salva arquivos. Pois na nuvem não existe estes arquivos!

--- src/testes/TesteDados.java
+++ src/testes/TesteDados.java
@@ -1,11 +1,5 @@
 package testes;
 
-import main.app.futebol.Jogador;
-import main.app.futebol.JogadorSerializavel;
-import main.libs.estruturas.Dados;
-import main.libs.estruturas.Lista;
-import main.libs.estruturas.fmt;
-import main.libs.tempo.Tempo;
 import main.libs.teste.TestandoCalebe;
 import main.libs.teste.TesteCalebe;
 import main.libs.teste.TesteClasseCalebe;
@@ -14,8 +8,10 @@ import main.libs.teste.TesteClasseCalebe;
 public class TesteDados {
 
     @TesteCalebe
-    public static void io_ler_arquivo(TestandoCalebe teste){
+    public static void io_ler_arquivo(TestandoCalebe teste) {
+        /*
         Lista<Jogador> jogadores = new Lista<Jogador>();
+
 
         jogadores.adicionar(new Jogador("Calebe",Tempo.parseData("23/03/2002"),10,"Atacante", Tempo.getDataHoje(),Tempo.getDataHoje(),Tempo.getHorarioAgora(),Tempo.getHorarioAgora()));
         jogadores.adicionar(new Jogador("Luan",Tempo.parseData("23/03/2002"),7,"Meia", Tempo.getDataHoje(),Tempo.getDataHoje(),Tempo.getHorarioAgora(),Tempo.getHorarioAgora()));
@@ -38,5 +34,7 @@ public class TesteDados {
         for (int i = 0; i < jogadores.getQuantidade(); i++) {
             teste.deveSerIgual(jogadores.get(i).getNome(), escola.get(i).getNome(),"io_ler_arquivo -> Teste_2");
         }
+
+         */
     }
 }
